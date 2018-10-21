db3ee51bcced625cc3683f6121e565ccd5235155
เอกบุรุษ ระวังงาน <egburut@gmail.com>
2018-10-21 14:06:33 +0700

    tes2

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/products/:pid', function (req, res) {
 
 });
 
-app.get('/users/:pid', function (req, res) {
+app.get('/users/:id', function (req, res) {
     var pid = req.params.id;
     var time = moment().format('MMMM Do YYYY, h:mm:ss a');
     var sql = "select * from users where id=" + id;
